9a3a69296c402b2552672b51c0bbc9ad48e21839
David Cauz <cauz.david@gmail.com>
2018-05-24 21:47:46 -0400

    test divss

--- test/asm/divss.s
+++ test/asm/divss.s
@@ -1,1 +1,8 @@
-divss
+divss %xmm2, %xmm7
+divss (%r10), %xmm0
+
+vdivss 0x10(%r10,%r12,1),  %xmm31, %xmm10
+vdivss 0x10(%r10,%r12,1),  %xmm5,  %xmm3
+vdivss 0x10(%r10,%r12,1),  %xmm5,  %xmm3
+
+vdivss %xmm7,%xmm25,%xmm10
