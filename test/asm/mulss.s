b5b31268f1fb2b329ad13e6b95ce4d15590fa8ec
David Cauz <cauz.david@gmail.com>
2018-05-24 21:26:25 -0400

    test mulss

--- test/asm/mulss.s
+++ test/asm/mulss.s
@@ -1,1 +1,8 @@
-mulss
+mulss %xmm2, %xmm7
+mulss (%r10), %xmm0
+
+vmulss 0x10(%r10,%r12,1),  %xmm31, %xmm10
+vmulss 0x10(%r10,%r12,1),  %xmm5,  %xmm3
+vmulss 0x10(%r10,%r12,1),  %xmm5,  %xmm3
+
+vmulss %xmm7,%xmm25,%xmm10
