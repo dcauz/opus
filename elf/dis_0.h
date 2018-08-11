3e87b8d04600902c4968a32f8b901bb5b6e938ca
David Cauz <cauz.david@gmail.com>
2018-08-11 10:54:47 -0400

    test pmaddubsw

--- elf/dis_0.h
+++ elf/dis_0.h
@@ -118,10 +118,22 @@ const char * dis_03(const char * code, unsigned prefix)
 
 const char * dis_04(const char * code, unsigned prefix)
 {
-	std::string op1;
-	std::string op2;
-	code = mod_reg_rm_ops( code, prefix, OpRegs::AL, 0, op1, op2 );	
-TODO
+    std::string op1;
+ 	std::string op2;
+
+	int vvvv = prefix >> 28;
+	vvvv = vvvv ^ 0xf;
+
+	if( prefix & PRE_256 )
+	{
+      	code = mod_reg_rm_ops( code, prefix, OpRegs::YMM0, 0, op1, op2 );
+    	printf( "vpmaddubsw %s,%%ymm%d,%s\n", op2.c_str(), vvvv, op1.c_str() );
+	}
+	else
+	{
+    	code = mod_reg_rm_ops( code, prefix, OpRegs::XMM0, 0, op1, op2 );
+    	printf( "vpmaddubsw %s,%%xmm%d,%s\n", op2.c_str(), vvvv, op1.c_str() );
+	}
 	return code;
 }
 
@@ -1022,6 +1034,15 @@ const char * dis_0f(const char * code, unsigned prefix)
 			else
 				code = mod_reg_rm_ops( code, prefix, OpRegs::MM0, 0, op1, op2 );
 			printf( "phaddsw %s,%s\n", op2.c_str(), op1.c_str() );
+		}
+		else if( code[1] == 4 )
+		{
+			code += 2;
+			if( prefix & PRE_OS )
+				code = mod_reg_rm_ops( code, prefix, OpRegs::XMM0, 0, op1, op2 );
+			else
+				code = mod_reg_rm_ops( code, prefix, OpRegs::MM0, 0, op1, op2 );
+			printf( "pmaddubsw %s,%s\n", op2.c_str(), op1.c_str() );
 		}
 		else if( code[1] == 5 )
 		{
--- test/asm/pmaddubsw.s
+++ test/asm/pmaddubsw.s
@@ -1,1 +1,69 @@
-pmaddubsw
+pmaddubsw %mm1, %mm5
+pmaddubsw %mm1, %mm7
+
+pmaddubsw %xmm1, %xmm5
+pmaddubsw %xmm1, %xmm7
+
+pmaddubsw (%r11), %xmm5
+pmaddubsw (%r11), %xmm15
+
+vpmaddubsw %xmm1, %xmm5, %xmm4
+vpmaddubsw %xmm1, %xmm15, %xmm4
+vpmaddubsw %xmm1, %xmm15, %xmm14
+vpmaddubsw %xmm1, %xmm15, %xmm24
+
+vpmaddubsw (%r11), %xmm5, %xmm4
+vpmaddubsw (%r11), %xmm15, %xmm4
+vpmaddubsw (%r11), %xmm15, %xmm14
+vpmaddubsw (%r11), %xmm15, %xmm24
+
+vpmaddubsw %ymm1, %ymm5, %ymm4
+vpmaddubsw %ymm1, %ymm15, %ymm4
+vpmaddubsw %ymm1, %ymm15, %ymm14
+vpmaddubsw %ymm1, %ymm15, %ymm24
+
+vpmaddubsw (%r11), %ymm5, %ymm4
+vpmaddubsw (%r11), %ymm15, %ymm4
+vpmaddubsw (%r11), %ymm15, %ymm14
+vpmaddubsw (%r11), %ymm15, %ymm24
+
+vpmaddubsw %zmm1, %zmm5, %zmm4
+vpmaddubsw %zmm1, %zmm15, %zmm4
+vpmaddubsw %zmm1, %zmm15, %zmm14
+vpmaddubsw %zmm1, %zmm15, %zmm24
+
+vpmaddubsw (%r11), %zmm5, %zmm4
+vpmaddubsw (%r11), %zmm15, %zmm4
+vpmaddubsw (%r11), %zmm15, %zmm14
+vpmaddubsw (%r11), %zmm15, %zmm24
+
+vpmaddubsw %xmm1, %xmm5, %xmm4{%k6}
+vpmaddubsw %xmm1, %xmm15, %xmm4{%k6}
+vpmaddubsw %xmm1, %xmm15, %xmm14{%k6}
+vpmaddubsw %xmm1, %xmm15, %xmm24{%k6}
+
+vpmaddubsw (%r11), %xmm5, %xmm4{%k6}
+vpmaddubsw (%r11), %xmm15, %xmm4{%k6}
+vpmaddubsw (%r11), %xmm15, %xmm14{%k6}
+vpmaddubsw (%r11), %xmm15, %xmm24{%k6}
+
+vpmaddubsw %ymm1, %ymm5, %ymm4{%k6}
+vpmaddubsw %ymm1, %ymm15, %ymm4{%k6}
+vpmaddubsw %ymm1, %ymm15, %ymm14{%k6}
+vpmaddubsw %ymm1, %ymm15, %ymm24{%k6}
+
+vpmaddubsw (%r11), %ymm5, %ymm4{%k6}
+vpmaddubsw (%r11), %ymm15, %ymm4{%k6}
+vpmaddubsw (%r11), %ymm15, %ymm14{%k6}
+vpmaddubsw (%r11), %ymm15, %ymm24{%k6}
+
+vpmaddubsw %zmm1, %zmm5, %zmm4{%k6}
+vpmaddubsw %zmm1, %zmm15, %zmm4{%k6}
+vpmaddubsw %zmm1, %zmm15, %zmm14{%k6}
+vpmaddubsw %zmm1, %zmm15, %zmm24{%k6}
+
+vpmaddubsw (%r11), %zmm5, %zmm4{%k6}
+vpmaddubsw (%r11), %zmm15, %zmm4{%k6}
+vpmaddubsw (%r11), %zmm15, %zmm14{%k6}
+vpmaddubsw (%r11), %zmm15, %zmm24{%k6}
+
