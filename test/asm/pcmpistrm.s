cf7e86ceec6093ae55f7b6699eeae31e3657f080
David Cauz <cauz.david@gmail.com>
2018-08-21 22:06:27 -0400

    test pcmpistrm

--- test/asm/pcmpistrm.s
+++ test/asm/pcmpistrm.s
@@ -1,1 +1,16 @@
-pcmpistrm
+pcmpistrm $1, %xmm1, %xmm2
+pcmpistrm $11, %xmm1, %xmm12
+pcmpistrm $111, %xmm1, %xmm12
+
+pcmpistrm $1, (%r10), %xmm2
+pcmpistrm $11, (%r10), %xmm12
+pcmpistrm $111, (%r10), %xmm12
+
+vpcmpistrm $1, %xmm1, %xmm2
+vpcmpistrm $11, %xmm1, %xmm12
+vpcmpistrm $111, %xmm1, %xmm12
+
+vpcmpistrm $1, (%r10), %xmm2
+vpcmpistrm $11, (%r10), %xmm12
+vpcmpistrm $111, (%r10), %xmm12
+
