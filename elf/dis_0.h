63b87d2ef9f25da242d43e1577560a4ff8191f31
David Cauz <cauz.david@gmail.com>
2018-08-22 07:18:59 -0400

    test aesdec

--- elf/dis_0.h
+++ elf/dis_0.h
@@ -1677,6 +1677,12 @@ const char * dis_0f(const char * code, unsigned prefix)
 			   	printf( "adcx %s,%s\n", op1.c_str(), op2.c_str() );
 			}
 		}
+		else if( code[1] == 0xffffffde )
+		{
+			code += 2;
+			code = mod_reg_rm_ops( code, prefix, OpRegs::XMM0, 0, op2, op1 );
+			printf( "aesdec %s,%s\n", op1.c_str(), op2.c_str() );
+		}
 		break;
 	}
 	case 0x3a:
--- test/asm/aesdec.s
+++ test/asm/aesdec.s
@@ -1,1 +1,10 @@
-aesdec
+aesdec %xmm1, %xmm2
+aesdec (%r10), %xmm2
+aesdec %xmm1, %xmm12
+aesdec (%r10), %xmm12
+
+vaesdec %xmm1, %xmm2, %xmm7
+vaesdec (%r10), %xmm2, %xmm7
+vaesdec %xmm1, %xmm12, %xmm7
+vaesdec (%r10), %xmm12, %xmm7
+
