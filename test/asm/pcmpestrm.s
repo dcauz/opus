e25d3467bea4f80e0cdaeb141ef5a2208556a90a
David Cauz <cauz.david@gmail.com>
2018-08-21 21:35:59 -0400

    test pcmpestrm

--- test/asm/pcmpestrm.s
+++ test/asm/pcmpestrm.s
@@ -1,1 +1,16 @@
-pcmpestrm
+pcmpestrm $1, %xmm1, %xmm2
+pcmpestrm $11, %xmm1, %xmm12
+pcmpestrm $111, %xmm1, %xmm12
+
+pcmpestrm $1, (%r10), %xmm2
+pcmpestrm $11, (%r10), %xmm12
+pcmpestrm $111, (%r10), %xmm12
+
+vpcmpestrm $1, %xmm1, %xmm2
+vpcmpestrm $11, %xmm1, %xmm12
+vpcmpestrm $111, %xmm1, %xmm12
+
+vpcmpestrm $1, (%r10), %xmm2
+vpcmpestrm $11, (%r10), %xmm12
+vpcmpestrm $111, (%r10), %xmm12
+
